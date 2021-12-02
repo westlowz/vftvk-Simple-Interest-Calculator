e44eb224d746ad49baee8d638061f52c4b04c62f
Peter <47428143+westlowz@users.noreply.github.com>
2021-12-02 09:16:46 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+  var principal = document.getElementById("principal").value;
+  var rate = document.getElementById("rate").value;
+  var years = document.getElementById("years").value;
+  var interest = (principal * years * rate) / 100;
+  var year = new Date().getFullYear() + parseInt(years);
+  if (principal <= 0) {
+    alert("Enter a positive number.");
+  }
+  document.getElementById("result").innerHTML =
+    "If you deposit " +
+    "<b>" +
+    principal +
+    "</b>" +
+    ",<br>at an interest rate of " +
+    "<b>" +
+    rate +
+    "</b>" +
+    "%<br>You will receive an amount of " +
+    "<b>" +
+    interest +
+    "</b>" +
+    ",<br>in the year " +
+    "<b>" +
+    year +
+    "</b>" +
+    "<br>";
+  focus("principal");
 }
-        
+
+function updateRate() {
+  var rateval = document.getElementById("rate").value;
+  document.getElementById("rate_val").innerText = rateval + "%";
+}
